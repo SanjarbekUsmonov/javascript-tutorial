227c08b8de3e7b21d1a4fa43d08d7e8321060983
SanjarbekUsmonov <noobpusk@gmail.com>
2022-11-20 23:06:14 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,171 @@
+// Function bilan ishlash 1
+// function aa (a){
+//    return a * 2
+// }
+// console.log(aa(5));
+
+
+//BU FOR LUP DA  2
+
+// for(let i = 0; i < array.length; i++){
+//     newArray.push(array[i])
+// }
+// console.log(newArray);
+
+
+
+//FOREACH FOR LUP BILAN BIR XIL LEKIN SODDAROQ
+// let array = [1,2,3,4,5,6,7,8,9]
+// let newArray = []
+
+// array.forEach(function(elem){
+//     newArray.push(elem)
+// })
+
+// console.log(newArray);
+
+
+//BU FOREACH DA ARRAYLAR BILAN FOYDALANISH 3
+// let newMalumot = []
+// let malumot = [
+//    {
+//       ismi:  "Sanjar",
+//       familyasi: "Usmonaliyev",
+//       yoshi: 22,
+//       email: "sanjari.@gmail.com"
+//    },
+//    {
+//       ismi:  "Jon Doe",
+//       familyasi: "Jon",
+//       yoshi: 30,
+//       email: "joniy.@gmail.com"
+//    },
+//    {
+//       ismi:  "Abdullox",
+//       familyasi: "Karimov",
+//       yoshi: 10,
+//       email: "abdull.@gmail.com"
+//    },
+// ]
+
+// malumot.forEach(function(elem){
+//    if(elem.yoshi > 10 ){
+//         newMalumot.push(elem.ismi)
+//    }
+// })
+// console.log(newMalumot);
+
+
+//BU FOR LUP DA ARRAY LAR BILAN FOYDALANISH 4
+// let newMalumot = []
+// let malumot = [
+//    {
+//       ismi:  "Sanjar",
+//       familyasi: "Usmonaliyev",
+//       yoshi: 22,
+//       email: "sanjari.@gmail.com"
+//    },
+//    {
+//       ismi:  "Jon Doe",
+//       familyasi: "Jon",
+//       yoshi: 30,
+//       email: "joniy.@gmail.com"
+//    },
+//    {
+//       ismi:  "Abdullox",
+//       familyasi: "Karimov",
+//       yoshi: 10,
+//       email: "abdull.@gmail.com"
+//    },
+// ]
+// for(let i = 0; i < malumot.length; i++){
+//      if(malumot[i].yoshi > 0){
+//          newMalumot.push(malumot[i].yoshi)
+//      }
+    
+// }
+// console.log(newMalumot);
+
+//MAP haqida tushuncha 5
+// let array = [1,2,3,4,5,6,7,8,9]
+
+// a = array.map(function(elem){
+//     return elem * elem
+// })
+// console.log(a);
+
+// MASSIV va MAP haqida tushuncha 6
+// let malumot = [
+//    {
+//       ismi:  "Sanjar",
+//       familyasi: "Usmonaliyev",
+//       yoshi: 22,
+//       email: "sanjari.@gmail.com",
+//       uylanganmi: false
+//    },
+//    {
+//       ismi:  "Jon Doe",
+//       familyasi: "Jon",
+//       yoshi: 30,
+//       email: "joniy.@gmail.com",
+//       uylanganmi: true
+//    },
+//    {
+//       ismi:  "Abdullox",
+//       familyasi: "Karimov",
+//       yoshi: 10,
+//       email: "abdull.@gmail.com",
+//       uylanganmi: false
+//    },
+// ]
+// a = malumot.map(elem=>{     
+//     return{
+//         ismlari: elem.ismi,
+//         yoshlari: elem.yoshi
+
+//     }
+// })
+// console.log(a);
+//qo'shimcha bo'sh bo'lagan massivga tenglashtirsa ham bo'ladi "a" ni o'rniga
+
+
+//Massivda Filtrni ishlatish  7
+
+// let array = [1,2,3,4,5,6,7,8,9, -1,-2,-3,-4]
+// a = array.filter(elem=>{
+//     return elem > 4
+// })
+// console.log(a);
+//qo'shimcha bo'sh bo'lagan massivga tenglashtirsa ham bo'ladi "a" ni o'rniga
+
+
+// let malumot = [
+//        {
+//           ismi:  "Sanjar",
+//           familyasi: "Usmonaliyev",
+//           yoshi: 22,
+//           email: "sanjari.@gmail.com",
+//           uylanganmi: false
+//        },
+//        {
+//           ismi:  "Jon Doe",
+//           familyasi: "Jon",
+//           yoshi: 30,
+//           email: "joniy.@gmail.com",
+//           uylanganmi: true
+//        },
+//        {
+//           ismi:  "Abdullox",
+//           familyasi: "Karimov",
+//           yoshi: 10,
+//           email: "abdull.@gmail.com",
+//           uylanganmi: false
+//        },
+    // ]
+
+    // a = malumot.filter(function(elem){
+       // return elem.uylanganmi           //Filter faqat true ni ko'radi
+    //    return elem.yoshi > 0
+    // })
+    // console.log(a);
+    //qo'shimcha bo'sh bo'lagan massivga tenglashtirsa ham bo'ladi "a" ni o'rniga
