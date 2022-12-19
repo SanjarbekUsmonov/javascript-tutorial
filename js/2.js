65948f1956a93858e99ad870f12acd77e6be9d82
SanjarbekUsmonov <noobpusk@gmail.com>
2022-12-19 06:58:48 +0500

    filter

---
+++ js/2.js
@@ -0,0 +1,52 @@
+//Map 
+
+// let array = [1,2,3,4,5,6,7,8,9]
+// let newArray = []
+
+// newArray = array.map(elem => {
+//     return elem *= elem
+// })
+
+// console.log(newArray);
+
+//Map orqali objektlarni olish
+// let person = [
+//     {
+//         name: 'Sanjar',
+//         sName: 'Usmonaliyev',
+//         age: 21,
+//         isMerid: false,
+//         from: 'Uzbekistan'
+//     },
+//     {
+//         name: 'Ahad',
+//         sName: 'Musayev',
+//         age: 29,
+//         isMerid: true,
+//         from: 'Tajikistan'
+//     },
+//     {
+//         name: 'Orzu',
+//         sName: 'Boltaboyev',
+//         age: 17,
+//         isMerid: false,
+//         from: 'Uzbekistan'
+//     },
+//     {
+//         name: 'Abdullox',
+//         sName: 'Yusupov',
+//         age: 30,
+//         isMerid: true,
+//         from: 'Qazaqistan'
+//     },
+// ]
+// let newArray= []
+
+// newArray = person.map(function(elem){
+//     return{
+//         name: elem.name,
+//         isMerid: elem.isMerid,
+//         from: elem.from
+//     }
+// })
+// console.log(newArray);
---
+++ js/3.js
@@ -0,0 +1,62 @@
+//filter 4dan katta sonlarni ko'rsatish
+
+// let array = [1,2,3,4,5,6,7,8,9]
+// let newArray = []
+
+// newArray = array.filter(function(elem){
+//     return elem > 4
+// })
+// console.log(newArray);
+
+
+
+
+//filter 0 dan katta va 0 dan kichik sonlarni topish
+
+// let array = [1,2,3,4,5,6,7,8,9,-1,-2,-3,-4,-5,-6,-7,-8,-9]
+// let newArray = []
+
+// newArray = array.filter(function(elem){
+//     return elem > 0
+//     return elem < 0
+// })
+// console.log(newArray);
+
+//filterda objektdagi bor bo'lgan elementni olish. filter faqat truni ko'radi
+// let person = [
+//     {
+//         name: 'Sanjar',
+//         sName: 'Usmonaliyev',
+//         age: 21,
+//         isMerid: false,
+//         from: 'Uzbekistan'
+//     },
+//     {
+//         name: 'Ahad',
+//         sName: 'Musayev',
+//         age: 29,
+//         isMerid: true,
+//         from: 'Tajikistan'
+//     },
+//     {
+//         name: 'Orzu',
+//         sName: 'Boltaboyev',
+//         age: 17,
+//         isMerid: false,
+//         from: 'Uzbekistan'
+//     },
+//     {
+//         name: 'Abdullox',
+//         sName: 'Yusupov',
+//         age: 30,
+//         isMerid: true,
+//         from: 'Qazaqistan'
+//     },
+// ]
+// let newArray= []
+
+// newArray = person.filter(function(elem){
+//     return elem.isMerid
+// })
+// console.log(newArray);
+
