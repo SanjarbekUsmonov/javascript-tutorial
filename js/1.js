0b5ae6ccf2f5cfcb8127e8d400249f4d99047135
SanjarbekUsmonov <noobpusk@gmail.com>
2022-12-19 06:02:54 +0500

    forlup

---
+++ js/1.js
@@ -0,0 +1,60 @@
+// MASSAIVLAR BILAN ISHLASH
+
+//forEach
+// const array = [1,2,3,4,5,6,7,8,9]
+// const newArray = []
+// array.forEach(function(elem){
+//     newArray.push(elem)
+// })
+// console.log(newArray);
+
+
+
+//for lup
+
+
+
+
+
+
+//ForEach sonni o'ziga ko'paytirish
+// const array = [1,2,3,4,5,6,7,8,9]
+// const newArray = []
+
+// array.forEach(function(elem){
+//     newArray.push(elem * elem)
+// })
+
+// console.log(newArray);
+
+
+let person = [
+    {
+        name: 'Sanjar',
+        sName: 'Usmonaliyev',
+        age: 21,
+        isMerid: false,
+        from: 'Uzbekistan'
+    },
+    {
+        name: 'Ahad',
+        sName: 'Musayev',
+        age: 29,
+        isMerid: true,
+        from: 'Tajikistan'
+    },
+    {
+        name: 'Orzu',
+        sName: 'Boltaboyev',
+        age: 17,
+        isMerid: false,
+        from: 'Uzbekistan'
+    },
+    {
+        name: 'Abdullox',
+        sName: 'Yusupov',
+        age: 30,
+        isMerid: true,
+        from: 'Qazaqistan'
+    },
+]
