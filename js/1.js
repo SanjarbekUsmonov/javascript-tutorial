c7e3472e71b904a8489b724a964f2d362238e3ce
SanjarbekUsmonov <noobpusk@gmail.com>
2022-12-19 06:28:59 +0500

    forEach

--- js/1.js
+++ js/1.js
@@ -11,9 +11,22 @@
 
 
 //for lup
+// const array = [1,2,3,4,5,6,7,8,9]
+// const newArray = []
 
+// for(let i = 0; i < array.length; i++){
+//     newArray.push(array[i])
+// }
+// console.log(newArray);
 
+//for of
+// let array = [1,2,3,4,5,6,7,8,9]
+// let newArray = []
 
+// for(let arrays of array){
+//     newArray.push(arrays)
+// }
+// console.log(newArray);
 
 
 
@@ -28,33 +41,44 @@
 // console.log(newArray);
 
 
-let person = [
-    {
-        name: 'Sanjar',
-        sName: 'Usmonaliyev',
-        age: 21,
-        isMerid: false,
-        from: 'Uzbekistan'
-    },
-    {
-        name: 'Ahad',
-        sName: 'Musayev',
-        age: 29,
-        isMerid: true,
-        from: 'Tajikistan'
-    },
-    {
-        name: 'Orzu',
-        sName: 'Boltaboyev',
-        age: 17,
-        isMerid: false,
-        from: 'Uzbekistan'
-    },
-    {
-        name: 'Abdullox',
-        sName: 'Yusupov',
-        age: 30,
-        isMerid: true,
-        from: 'Qazaqistan'
-    },
-]
+
+//forEach da objectsagi malumotni olish
+// let person = [
+//     {
+//         name: 'Sanjar',
+//         sName: 'Usmonaliyev',
+//         age: 21,
+//         isMerid: false,
+//         from: 'Uzbekistan'
+//     },
+//     {
+//         name: 'Ahad',
+//         sName: 'Musayev',
+//         age: 29,
+//         isMerid: true,
+//         from: 'Tajikistan'
+//     },
+//     {
+//         name: 'Orzu',
+//         sName: 'Boltaboyev',
+//         age: 17,
+//         isMerid: false,
+//         from: 'Uzbekistan'
+//     },
+//     {
+//         name: 'Abdullox',
+//         sName: 'Yusupov',
+//         age: 30,
+//         isMerid: true,
+//         from: 'Qazaqistan'
+//     },
+// ]
+// let newArray= []
+
+// let getName = function(elem){
+//     newArray.push(elem.name)
+// }
+
+// person.forEach(getName)
+// console.log(newArray);
+
