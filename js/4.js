6a9fa81c3bb08e21300a9af1ab9c71bf2d4725e3
SanjarbekUsmonov <noobpusk@gmail.com>
2022-12-21 10:27:35 +0500

    some and every , reduce

---
+++ js/4.js
@@ -0,0 +1,53 @@
+//Massiv da some every metodi
+
+// let array = [1,2,3,4,5,6,7,8,9,-1,-2,-3,-4,-5,-6,-7,-8,-9]
+// let newArray = []
+
+// newArray = array.some(function(elem){
+//     return elem > 0
+// })
+// console.log(newArray);
+
+// newArray = array.every(function(elem){
+//     return elem > 0
+// })
+// console.log(newArray);
+
+
+// let person = [
+//         {
+//             name: 'Sanjar',
+//             sName: 'Usmonaliyev',
+//             age: 21,
+//             isMerid: false,
+//             from: 'Uzbekistan'
+//         },
+//         {
+//             name: 'Ahad',
+//             sName: 'Musayev',
+//             age: 29,
+//             isMerid: true,
+//             from: 'Tajikistan'
+//         },
+//         {
+//             name: 'Orzu',
+//             sName: 'Boltaboyev',
+//             age: 17,
+//             isMerid: false,
+//             from: 'Uzbekistan'
+//         },
+//         {
+//             name: 'Abdullox',
+//             sName: 'Yusupov',
+//             age: 30,
+//             isMerid: true,
+//             from: 'Qazaqistan'
+//         },
+//     ]
+//     let newArray= []
+
+//     newArray = person.some(function(elem){  //some true ni yani arrayda bor bo'lgan malumotlarni ko'radi  
+        // return elem.from === "Qazaqistan"    // every esa arraydagi objectning barchasida  
+    // })                                       //malutom bo'lsayu faqat bittasida o'sha malumot bo'lmasa unda false 
+    // console.log(newArray);
+    
---
+++ js/5.js
@@ -0,0 +1,12 @@
+// reduce
+
+// let array = [2,2,2,2,-2,-2]
+// let newArray = []
+
+// newArray = array.reduce(function(sum,elem){
+//     return sum +=elem
+// }, 10)                          // 10 sum ni o'ziga qabul qiladi
+// console.log(newArray);
+
+
+// reduce praktika
