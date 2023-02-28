2b541e478efba90eed3c76b93953c065ea38c703
SanjarbekUsmonov <noobpusk@gmail.com>
2023-03-01 01:48:03 +0500

    object type

--- js/8.js
+++ js/8.js
@@ -24,9 +24,20 @@
 // let newArray = Array.from(new Set(array))
 // console.log(newArray);
 
-let array = [3,2,4,5,1,8,6,7]
-let newArray = []
 
 // const points = [40, 100, 1, 5, 25, 10];
 // points.sort(function(a, b){return a - b});
 // console.log(points);
+
+// const fruit = [1,4,2,5,3,]
+// fruit.sort()
+// console.log(fruit)
+
+let user = {name:'Sanjar',age:'22',email:'abrakadabra@gmail.com'}
+// let value = Object.keys(user)
+// console.log(value);
+let value = []
+for(let i = 0; i < user.lenght; i++){
+    value.push(user.lenght)
+}
+console.log(value);
